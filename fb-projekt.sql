8c42f6bf05c49a65e51d6a56da4f491b2fb81915
richtar <ondrej.richtar@konrad-technologies.de>
2015-03-02 17:17:40 +0100

    Updated database

--- fb-projekt.sql
+++ fb-projekt.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2015 at 01:43 PM
+-- Generation Time: Mar 02, 2015 at 05:16 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -23,6 +23,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+`id` int(10) NOT NULL,
+  `comment` varchar(500) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `comment`) VALUES
+(1, 'was');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `websiteusers`
 --
 
@@ -31,19 +49,27 @@ CREATE TABLE IF NOT EXISTS `websiteusers` (
   `fullname` varchar(50) NOT NULL,
   `userName` varchar(40) NOT NULL,
   `email` varchar(40) NOT NULL,
-  `pass` varchar(40) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `pass` varchar(40) NOT NULL,
+  `online` tinyint(2) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `websiteusers`
 --
 
-INSERT INTO `websiteusers` (`userID`, `fullname`, `userName`, `email`, `pass`) VALUES
-(4, 'Ondrej Richtar', 'Testuser', 'bla@test.com', '12345');
+INSERT INTO `websiteusers` (`userID`, `fullname`, `userName`, `email`, `pass`, `online`) VALUES
+(4, 'Ondrej Richtar', 'Testuser', 'bla@test.com', '12345', 0),
+(12, 'Hans', 'Brot', 'brot@mail.de', 'deimuddi', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `websiteusers`
@@ -56,10 +82,15 @@ ALTER TABLE `websiteusers`
 --
 
 --
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `websiteusers`
 --
 ALTER TABLE `websiteusers`
-MODIFY `userID` int(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+MODIFY `userID` int(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
